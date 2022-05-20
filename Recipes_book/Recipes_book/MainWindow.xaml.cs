a10f46f1e48f7796cdb77f8e8adbabae417120bb
Magnitik1 <aleksaleks23122004@gmail.com>
2022-05-20 23:18:14 +0200

    Recipes-Book

---
+++ Recipes_book/Recipes_book/MainWindow.xaml.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+
+namespace Recipes_book;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window {
+    public MainWindow() {
+        InitializeComponent();
+        update();
+    }
+    private void NumberValidationTextBox(object sender, TextCompositionEventArgs e) {
+        Regex regex = new Regex("[^0-9]+");
+        e.Handled = regex.IsMatch(e.Text);
+    }
+    public static List<string> files = new List<string>();
+    public static string min_t;
+    public static string max_t;
+    public static string main_ingr;
+    public static string type;
+    public void update() {
+        var t = Directory.GetFiles("ingr/");
+        q.Items.Clear();
+        files.Clear();
+        foreach (var f in t) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 5; i < f.Length - 4; i++) { sb.Append(f[i]); }
+            files.Add(sb.ToString());
+            q.Items.Add(sb.ToString());
+        }
+        q.FontSize = 25;
+        ListBoxItem item = new ListBoxItem() {
+            Content = " +  Add New Recipe",
+            Foreground = new SolidColorBrush(Colors.LimeGreen),
+            Height = 60,
+            FontWeight = FontWeights.Bold
+        };
+        q.Items.Add(item);
+    }
+    public static string way;
+    private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+        if (q.Items.Count <= 0) return;
+        string s = (sender as ListBox).SelectedValue.ToString();
+        if (s == "System.Windows.Controls.ListBoxItem:  +  Add New Recipe") {
+            Window1 win = new Window1();
+            win.but.Visibility = Visibility.Hidden;
+            win.cancel.Visibility = Visibility.Visible;
+            win.save.Visibility = Visibility.Visible;
+            win.name.IsReadOnly = false;
+            win.ingridients.IsReadOnly = false;
+            win.recipe.IsReadOnly = false;
+            win.time.IsReadOnly = false;
+            win.type.IsEnabled = true;
+            Window1.AddNew = true;
+            Close();
+            win.Show();
+        }
+        else {
+            way = s;
+            StreamReader sr = new StreamReader("ingr/" + way + ".txt");
+            string all = sr.ReadToEnd();
+            sr.Close();
+            string name = GetName(all);
+            string ingridients = GetIngridients(all);
+            string recipe = GetRecipe(all);
+            string type3 = GetType(all);
+            string time = GetTime(all);
+            min_t = mint.Text;
+            max_t = maxt.Text;
+            type = type2.Text;
+            main_ingr = mainprod.Text.Trim();
+            Window1 win = new Window1();
+            win.name.Text = name;
+            win.recipe.Text = recipe;
+            win.ingridients.Text = ingridients;
+            win.time.Text = time;
+            win.type.Text = type3;
+            win.Show();
+            Close();
+        }
+    }
+
+    private string GetTime(string all) {
+        var temp = new StringBuilder();
+        int i = all.IndexOf(":&$&(") + 6;
+        int end = all.IndexOf("%^*$^&") - 1;
+        for (int y = i; i < end; i++) {
+            temp.Append(all[i]);
+        }
+        return new string(temp.ToString());
+    }
+
+    private string GetType(string all) {
+        var temp = new StringBuilder();
+        int i = all.IndexOf(":#$%^") + 6;
+        int end = all.IndexOf("(&$^&") - 1;
+        for (int y = i; i < end; i++) {
+            temp.Append(all[i]);
+        }
+        return new string(temp.ToString());
+    }
+
+    private string GetRecipe(string all) {
+        var temp = new StringBuilder();
+        int i = all.Trim('\r').IndexOf("*&*)(*&^") + 10;
+        int end = all.Length - 1;
+        for (int y = i; i < end; i++) {
+            temp.Append(all[i]);
+        }
+        return new string(temp.ToString());
+    }
+
+    private string GetIngridients(string all) {
+        var temp = new StringBuilder();
+        int i = all.Trim('\r').IndexOf("*#$%@") + 7;
+        int end = all.IndexOf("*&&*$%");
+        for (int y = i; i < end; i++) {
+            temp.Append(all[i]);
+        }
+        return new string(temp.ToString());
+    }
+
+    private string GetName(string all) {
+        var temp = new StringBuilder();
+        for (int i = 6; i < all.Length; i++) {
+            if (all[i] == '\r') break;
+            temp.Append(all[i]);
+        }
+        return new string(temp.ToString());
+    }
+    private void q_MouseDoubleClick(object sender, MouseButtonEventArgs e) {
+        ListBox_SelectionChanged(sender, null);
+    }
+
+    private void CleanF(object sender, RoutedEventArgs e) {
+        mint.Text = "";
+        maxt.Text = "";
+        mainprod.Text = "";
+        type2.SelectedIndex = 6;
+        update();
+    }
+
+    public void Search(object sender, RoutedEventArgs e) {
+        var t = Directory.GetFiles("ingr/");
+        q.Items.Clear();
+        foreach (var f in t) {
+            StreamReader sr = new StreamReader(f);
+            string s = sr.ReadToEnd();
+            int ti = Convert.ToInt32(GetTime(s).Trim('х', 'в', '.', ' '));
+            string ing = GetIngridients(s);
+            string typ = GetType(s);
+            bool no_min = false, no_max = false, no_ingr = false, no_type = false;
+            int mi = 99999, ma = 0;
+            if (mint.Text != null && mint.Text.Trim() != "") mi = Convert.ToInt32(mint.Text.Trim()); else no_min = true;
+            if (maxt.Text != null && maxt.Text.Trim() != "") ma = Convert.ToInt32(maxt.Text.Trim()); else no_max = true;
+            try {
+                if (mainprod.Text != null && mainprod.Text.Trim() != "")
+                    no_ingr = ing.ToLower().Contains(mainprod.Text.ToLower());
+                else no_ingr = true;
+            }
+            catch { no_ingr = true; }
+            if (type2.Text == typ || type2.Text == "Будь-які") { no_type = true; }
+            if ((ti >= mi || no_min) && (ti <= ma || no_max) && (no_ingr) && (no_type)) {
+                q.Items.Add(GetName(s));
+            }
+        }
+        ListBoxItem item = new ListBoxItem() {
+            Content = " +  Add New Recipe",
+            Foreground = new SolidColorBrush(Colors.LimeGreen),
+            Height = 60,
+            FontWeight = FontWeights.Bold
+        };
+        q.Items.Add(item);
+
+    }
+    private void random(object sender, RoutedEventArgs e) {
+        Random rnd = new Random();
+        q.SelectedIndex=rnd.Next(0,q.Items.Count-1);
+    }
+}
+
+
---
+++ Recipes_book/Recipes_book/Window1.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Recipes_book;
+/// <summary>
+/// Interaction logic for Window1.xaml
+/// </summary>
+public partial class Window1 : Window {
+    public Window1() {
+        InitializeComponent();
+    }
+    public static string orig_name;
+    public static string orig_ingridients;
+    public static string orig_recipe;
+    public static string orig_type;
+    public static string orig_time;
+    public static bool AddNew = false;
+    private void Button_Click(object sender, RoutedEventArgs e) {
+        but.Visibility = Visibility.Hidden;
+        orig_name = name.Text;
+        orig_ingridients = ingridients.Text;
+        orig_recipe = recipe.Text;
+        var sb = new StringBuilder();
+        int u = time.Text.IndexOf(" хв.");
+        for (int i = 0; i < u; i++) {
+            sb.Append(time.Text[i]);
+        }
+        orig_time = time.Text;
+        time.Text = sb.ToString();
+        orig_type = type.Text;
+        cancel.Visibility = Visibility.Visible;
+        save.Visibility = Visibility.Visible;
+        delete.Visibility = Visibility.Visible;
+        recipe.IsReadOnly = false;
+        ingridients.IsReadOnly = false;
+        name.IsReadOnly = false;
+        type.IsEnabled = true;
+        time.IsReadOnly = false;
+    }
+    private void cancel_Click(object sender, RoutedEventArgs e) {
+        name.Text = orig_name;
+        if(name.Text == "") {
+            Close();
+            return;
+        }
+        but.Visibility = Visibility.Visible;
+        name.IsReadOnly = true;
+        recipe.Text = orig_recipe;
+        recipe.IsReadOnly = true;
+        ingridients.Text = orig_ingridients;
+        ingridients.IsReadOnly = true;
+        type.Text = orig_type;
+        type.IsEnabled = false;
+        time.Text = orig_time;
+        time.IsReadOnly = true;
+        cancel.Visibility = Visibility.Hidden;
+        save.Visibility = Visibility.Hidden;
+        delete.Visibility = Visibility.Hidden;
+    }
+    private void NameValidationTextBox(object sender, TextCompositionEventArgs e) {
+        Regex regex = new Regex("[^a-z A-Z а-я А-Я 0-9 і ї І Ї]+");
+        e.Handled = regex.IsMatch(e.Text);
+    }
+    private void timeValidationTextBox(object sender, TextCompositionEventArgs e) {
+        Regex regex = new Regex("[^0-9]+");
+        e.Handled = regex.IsMatch(e.Text);
+    }
+    private void save_Click(object sender, RoutedEventArgs e) {
+        if(time.Text == null||time.Text=="") return;
+        if(ingridients.Text == null||ingridients.Text=="") return;
+        if(recipe.Text == null|| recipe.Text=="") return;
+        if(type.Text == null||type.Text=="") return;
+        if (name.Text.Trim() == "" || name.Text == null || (MainWindow.files.Contains(name.Text) || name.Text==" +  Add New Recipe") && (orig_name != name.Text||AddNew)) { return; }
+        AddNew = false;
+        time.Text = time.Text.Trim();
+        time.Text += " хв.";
+        if (name.Text != orig_name&&orig_name!=null) {
+            File.Delete("ingr/" + orig_name + ".txt");
+        }
+        StreamWriter sw = new StreamWriter("ingr/" + name.Text + ".txt");
+        sw.WriteLine("Name: " + name.Text);
+        sw.WriteLine("Ingridients: *#$%@\r " + ingridients.Text + "\r*&&*$%");
+        sw.WriteLine("Type:#$%^ " + type.Text + " (&$^&");
+        sw.WriteLine("Time:&$&( " + time.Text + " %^*$^&");
+        sw.WriteLine("Recepie: *&*)(*&^\r " + recipe.Text);
+        sw.Close();
+        Close();
+    }
+
+    private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e) {
+        MainWindow mw = new MainWindow();
+        if (MainWindow.min_t == null) mw.mint.Text = "";
+        else if (MainWindow.min_t.ToString() != "0") mw.mint.Text = MainWindow.min_t; 
+        if (MainWindow.max_t == null) mw.maxt.Text = "";
+        else if (MainWindow.max_t.ToString() != "0") mw.maxt.Text = MainWindow.max_t;
+        if (MainWindow.type == null) mw.type2.Text = "Будь-які";
+        else mw.type2.Text=MainWindow.type;
+        mw.mainprod.Text = MainWindow.main_ingr;
+        mw.Show();
+        mw.Search(null, null);
+    }
+    private void del(object sender, RoutedEventArgs e) {
+        if(MessageBox.Show("Are you shure?", "", MessageBoxButton.YesNo, MessageBoxImage.Error) == MessageBoxResult.Yes){
+            File.Delete("ingr/" + orig_name + ".txt");
+            Close();
+        }
+    }
+}
